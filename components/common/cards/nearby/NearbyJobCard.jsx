9d55bee8ae7ceff30588a5b69fdd68c5828341a2
jantarktic <jaydenclark@outlook.com.au>
2023-07-04 16:39:59 +1000

    add nearby jobs and nearby job card

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-import styles from './nearbyjobcard.style'
+import styles from "./nearbyjobcard.style";
+import { checkImageURL } from "../../../../utils";
 
-const NearbyJobCard = () => {
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      className="flex flex-row justify-between items-center p-4 bg-white shadow-lg rounded-2xl"
+      onPress={handleNavigate}
+    >
+      <TouchableOpacity className="w-12 h-12 bg-gray-100 rounded-xl justify-center items-center">
+        <Image
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode="contain"
+          className="w-3/4 h-3/4"
+        />
+      </TouchableOpacity>
 
-export default NearbyJobCard
+      <View className="flex mx-4">
+        <Text className="text-base font-bold text-indigo-900" numberOfLines={1}>
+          {job?.job_title}
+        </Text>
+
+        <Text className="text-sm font-sans text-gray-500 mt-1 capitalize">
+          {job?.job_employment_type}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NearbyJobCard;
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { useRouter } from "expo-router";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
-import styles from './nearbyjobs.style'
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Native developer",
+    num_pages: "1",
+  });
 
-export default Nearbyjobs
+  return (
+    <View className="mt-8">
+      <View className="mt-3 flex-row justify-between">
+        <Text className="text-3xl font-medium text-gray-500">Nearby jobs</Text>
+        <TouchableOpacity>
+          <Text className="text-base font-medium text-gray-500">Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ gap: 12 }} className="mt-4">
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#312651" />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
